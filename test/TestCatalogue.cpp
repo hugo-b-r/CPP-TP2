bff8cab8bbebdc510d53f4586fef9962fd83e9cf
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2025-12-06 18:39:49 +0100

    add more tests for complex trajet operations

--- test/TestCatalogue.cpp
+++ test/TestCatalogue.cpp
@@ -58,17 +58,65 @@ void TestCatalogue::testRechercheParcours1()
 {
   Test::TestHeader("Recherche Parcours 1", "Catalogue");
   cout << "1 : de Paris à Brest en Gateau" << std::endl;
+  cout << "1 : de Paris à Brest en Voiture" << std::endl;
   Catalogue C1 = Catalogue();
 
   TrajetSimple * traj1 = new TrajetSimple("Paris", "Brest", "Gateau");
   C1.AjouterTrajet(traj1);
+  traj1 = new TrajetSimple("Chambéry", "Lyon", "Bateau");
+  C1.AjouterTrajet(traj1);
+  traj1 = new TrajetSimple("Paris", "Brest", "Voiture");
+  C1.AjouterTrajet(traj1);
+  traj1 = new TrajetSimple("Paris", "Mest", "Gateau");
+  C1.AjouterTrajet(traj1);
+
   Test::etAffiche();
   C1.RechercheParcours1("Paris", "Brest");
 }
 
 void TestCatalogue::testRechercheParcours2()
 // Algo:
-{}
+{
+    Test::TestHeader(" Recherche parcours composes", "Catalogue");
+    Catalogue C1 = Catalogue();
+    TrajetSimple ts1 ("Chambéry", "Paris", "Velo");
+    TrajetSimple ts2 ("Paris", "Londres", "Voiture");
+    TrajetSimple ts3 ("Londres", "Marseille", "Train");
+    TrajetSimple *tab = new TrajetSimple[3];
+    tab[0] = ts1;
+    tab[1] = ts2;
+    tab[2] = ts3;
+    TrajetCompose *t1 = new TrajetCompose(tab, 3);
+    delete[] tab;
+    C1.AjouterTrajet(t1);
+
+    TrajetSimple ts12 ("Marseille", "Bruxelles", "Velo");
+    TrajetSimple ts22 ("Bruxelles", "Nantes", "Voiture");
+    TrajetSimple ts32 ("Nantes", " Rennes", "Train");
+    tab = new TrajetSimple[3];
+    tab[0] = ts12;
+    tab[1] = ts22;
+    tab[2] = ts32;
+    t1 = new TrajetCompose(tab, 3);
+    delete[] tab;
+    C1.AjouterTrajet(t1);
+
+    TrajetSimple *ts13 = new TrajetSimple ("Grenoble", "Paris", "Voiture");
+    C1.AjouterTrajet(ts13);
+    ts13 = new TrajetSimple ("Chambéry", "Rennes", "Voiture");
+    C1.AjouterTrajet(ts13);
+    // ce que le test doit afficher
+    cout << "Un trajet avec correspondances a été trouvé, composé des trajets suivants :" << endl;
+    cout << "de Chambéry à Paris en Velo - de Paris à Londres en Voiture - de Londres à Marseille en Train - " << endl;
+    cout << "de Marseille à Bruxelles en Velo - de Bruxelles à Nantes en Voiture - de Nantes à Rennes en Train - " << endl;
+    cout << "Un trajet avec correspondances a été trouvé, composé des trajets suivants :" << endl;
+    cout << "De Chambéry à Rennes en Voiture" << endl;
+
+    Test::etAffiche();
+    // ce qu'il affiche vraiment
+    C1.RechercheParcours2("Chambéry", "Rennes");
+
+}
 
 
 void TestCatalogue::testAjouterTrajet()
